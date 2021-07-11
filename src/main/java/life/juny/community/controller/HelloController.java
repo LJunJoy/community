8448a5b2040d5de2c1653dde12c7a4ecef8b4e7a
LJunJoy <904312309@qq.com>
2021-07-11 20:10:01 +0800

    init project

---
+++ src/main/java/life/juny/community/controller/HelloController.java
@@ -0,0 +1,22 @@
+package life.juny.community.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+/**
+ * @Description TODO
+ * @Author Juny
+ * @Date 2021/7/11 18:52
+ * @Version 1.0
+ */
+@Controller
+public class HelloController {
+
+  @GetMapping("/greeting")
+  public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
+    model.addAttribute("name", name);
+    return "greeting";
+  }
+}
