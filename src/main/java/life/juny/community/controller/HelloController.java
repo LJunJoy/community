e827c0a8b2a0d557c2769d1d36ec815729b7b37a
LJunJoy <904312309@qq.com>
2021-07-13 22:38:40 +0800

    使用bootstrap开发导航条

--- src/main/java/life/juny/community/controller/HelloController.java
+++ src/main/java/life/juny/community/controller/HelloController.java
@@ -14,6 +14,11 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 public class HelloController {
 
+  @GetMapping("/")
+  public String index() {
+    return "index";
+  }
+
   @GetMapping("/greeting")
   public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
     model.addAttribute("name", name);
